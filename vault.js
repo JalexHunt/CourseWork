1e02c7b246cb95b32dfc5c3492b049ce7f3a493e
John Cooper II <john.cooper.tw0@gmail.com>
2024-03-14 19:58:01 -0500

    uploading files

---
+++ vault.js
@@ -0,0 +1,10 @@
+//pseudocode: Tpye in Message for user
+//pseudocode: Assign three variables using arithmetic operator.
+//pseudocode: Create alert pop up to give code to user
+console.log("You have received this message because you have been chosen to open an important vault. Here is the secret combination:")
+//making the code using arithmatic operators
+let numOne= 5+5
+let numTwo=5*8
+let numThree=53-14
+//crearting popup alert that shows the code for the  vault
+alert(`${numOne} - ${numTwo} - ${numThree}`)
